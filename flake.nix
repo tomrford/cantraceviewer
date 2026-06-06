634aaa2937b70279f63c2b0c9ba6da3e926f5110
Tom Ford <t@tomrford.com>
2026-06-07 00:27:23 +0200

    adjustment to dev shell for zig version and language server addition (#49)

--- flake.nix
+++ flake.nix
@@ -5,29 +5,24 @@
 
     # Utility for building this flake
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Overlay for bringing in the zig compiler for the simulation DLL
-    zig-overlay.url = "github:mitchellh/zig-overlay";
-    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    zig-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [zig-overlay.overlays.default];
         };
       in {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.bun
-            pkgs.zigpkgs."0.16.0"
+            pkgs.zig_0_16
+            pkgs.zls_0_16
           ];
         };
       }
